78cc04fe37754019770521ad20bb591ce0d57cd7
zackmolt <fkrihkl@gmail.com>
2023-03-27 17:20:10 +0700

    Initial Commit

---
+++ RefreshTokenRenewal/RefreshTokenRenewalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefreshTokenRenewalApp.swift
+//  RefreshTokenRenewal
+//
+//  Created by Fikri Haikal on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RefreshTokenRenewalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
